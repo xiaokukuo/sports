1a4faa8b7e3ec0b6dd349e18bb01d547d72c621b
xiaokukuo <976886537@qq.com>
2018-08-06 14:56:35 +0800

    12

--- sports-parent/sports-batch/src/main/java/org/sports/batch/BatchApplication.java
+++ sports-parent/sports-batch/src/main/java/org/sports/batch/BatchApplication.java
@@ -2,6 +2,7 @@ package org.sports.batch;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.sports.core.MapperNameGenerator;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
@@ -11,6 +12,6 @@ import org.springframework.context.annotation.PropertySources;
 @MapperScan(value = { "org.sports.core.mapper" }, nameGenerator = MapperNameGenerator.class)
 public class BatchApplication {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		SpringApplication.run(BatchApplication.class, args);
 	}
 }
